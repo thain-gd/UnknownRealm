e7417bd9128c091807d639cf2d08ea33ba656441
Thai Nguyen <nqcuong2@gmail.com>
2021-05-06 19:50:08 -0500

    Init Unreal project

---
+++ Source/UnknownRealm/UnknownRealmGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnknownRealmGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNKNOWNREALM_API AUnknownRealmGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
